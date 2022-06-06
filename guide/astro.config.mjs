7b71166588a5d93ad363a98a44240faa103ffacc
David East <deast@google.com>
2022-06-06 15:49:57 -0400

    add site config

--- guide/astro.config.mjs
+++ guide/astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://frontend-masters-firebase.web.app/',
   server: {
     port: 3033,
   },
